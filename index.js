4743cc1e1135becda7e7a402d8b08e83535f9e43
ColbyCabrera <gdthyispro@gmail.com>
2023-02-01 10:36:01 -0500

    Create logic to send page url files

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    const url = new URL("https://localhost:8080" + req.url);
+
+    if (req.url != "/favicon.ico") {
+      const filename = "." + url.pathname;
+      console.log(filename);
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end(fs.readFileSync("./404.html", "utf8"));
+        }
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
+  })
+  .listen(8080);
